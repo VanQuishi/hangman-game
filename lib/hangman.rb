afc9087363bbb269d4b53d439f2f4b6e7366b624
Chau Nguyen <songchau99@gmail.com>
2020-05-30 00:06:40 -0500

    Recognize the problem with checking correct answer

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -9,16 +9,56 @@ class Hangman
     
         def initialize(player_name,misses=[],corrects=[],stage=0)
             @player_name = player_name
-            @key_word = find_key_word("5desk.txt")
+            @key_word = find_key_word("5desk.txt").chomp
+            #@key_word = "corpsman"
             @misses = misses
             @corrects = corrects
             @stage = stage
         end
 
+        def game()
+            puts "Hi #{@player_name} <3"
+            puts "Your word is #{@key_word.length}-letter long."
+            puts "You have #{6-@stage} lives left."
+            display_hangman()
+            display_corrects()
+            display_misses()
+
+            while @stage < 6 do 
+                p @corrects
+                p @misses
+
+                if @corrects.join().downcase == @key_word.downcase   #this condition has problem
+                    puts File.read("assets/winner.txt")
+                    break
+                end
+
+                print "Guess a letter: "
+                guess = gets.chomp.downcase
+                is_correct_guess = hit_or_miss(guess)
+
+                if is_correct_guess == true
+                    puts "Nice guess!!!"
+                else
+                    puts "Wrong :("
+                    @stage += 1
+                    #puts "You have #{6-@stage} lives left."
+                end
+
+                display_hangman()
+                display_corrects()
+                display_misses()
+
+                if @stage == 6
+                    puts "The secret word is #{@key_word}"
+                    break
+                end
+            end
+        end
+
     private
 
         def find_key_word(filename)
-
             lines = File.readlines(filename)
             word = lines[rand(lines.length)].chomp
         
@@ -29,10 +69,10 @@ class Hangman
             return word
         end
 
-        def display_corrects(word,corrects)
-            for i in 0..(word.length-1)
-                if corrects.include?(word[i].downcase)
-                    print "#{word[i]} "
+        def display_corrects()
+            for i in 0..(@key_word.length-1)
+                if @corrects.include?(@key_word[i].downcase)
+                    print "#{@key_word[i]} "
                 else
                     print '_ '
                 end
@@ -40,11 +80,75 @@ class Hangman
             puts "\n"
         end
 
-        def display_misses(misses)
-            misses_string = misses.join(', ')
-            p "misses: #{misses_string}"
+        def display_misses()
+            misses_string = @misses.join(', ')
+            puts "Misses: #{misses_string}"
+        end
+
+        def display_hangman()
+            if @stage == 0
+                puts File.read("assets/hm0.txt")
+            elsif @stage == 1
+                puts File.read("assets/hm1.txt")
+            elsif @stage == 2
+                puts File.read("assets/hm2.txt")
+            elsif @stage == 3
+                puts File.read("assets/hm3.txt")
+            elsif @stage == 4
+                puts File.read("assets/hm4.txt")
+            elsif @stage == 5
+                puts File.read("assets/hm5.txt")
+            elsif @stage == 6
+                puts File.read("assets/hm6.txt")
+            end
+        end
+
+        def hit_or_miss(guess)
+            if @key_word.downcase.include?(guess)
+                @corrects.push(guess)
+                return true
+            else
+                @misses.push(guess)
+                return false
+            end
         end
 end
+
+
+
+print "What is your name? "
+name = gets.chomp
+
+player = Hangman.new(name)
+puts "keyword: #{player.key_word}"
+player.game()
+
+
+
+=begin
+word = "Hangman"
+guess = 'z'
+
+misses = []
+corrects = []
+
+if word.downcase.include?(guess)
+    corrects.push(guess)
+else
+    misses.push(guess)
+end
+p corrects
+p misses
+=end
+
+=begin
+stage = 6
+if stage == 0
+    puts File.read("assets/hm0.txt")
+elsif stage == 6
+    puts File.read("assets/hm6.txt")
+end
+=end
 
 =begin
 nin = Hangman.new("nin")
